c393b4c74fd64254a39bb61912bf3aafe35f53c7
Michael Emmi <michael.emmi@gmail.com>
2015-06-05 22:56:52 +0200

    Created CV template.

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-mustache data/michaelemmi.yml layouts/profile.html > index.html && git add index.html
+mustache data/michaelemmi.yml layouts/index.mustache > index.html && git add index.html
+mustache data/michaelemmi.yml layouts/cv.mustache > cv.html && git add cv.html
