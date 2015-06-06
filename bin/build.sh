2c307c41c11ee761db78804e5af219747ad0e483
Michael Emmi <michael.emmi@gmail.com>
2015-06-06 14:55:06 +0200

    Minimalism.

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-mustache data/michaelemmi.yml layouts/index.mustache > index.html && git add index.html
-mustache data/michaelemmi.yml layouts/cv.mustache > cv.html && git add cv.html
+DATA="data/michaelemmi.yml"
+PAGES="index cv"
+
+for p in ${PAGES}
+do
+  echo "Building ${p}.html"
+  mustache ${DATA} layouts/${p}.mustache > ${p}.html && git add ${p}.html
+done
